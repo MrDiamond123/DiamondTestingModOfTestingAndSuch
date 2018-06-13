e25a9cfd32c4815e10761568a3601254fad645f3
Mr_Diamond123 <kurtisiscool123@gmail.com>
2018-06-12 20:14:14 -0400

    Testing and failing every day

--- src/main/java/diamond/dtmotas/blocks/BlockCompressedCrystal.java
+++ src/main/java/diamond/dtmotas/blocks/BlockCompressedCrystal.java
@@ -3,6 +3,11 @@ package diamond.dtmotas.blocks;
 import diamond.dtmotas.ModDtmotas;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 
 public class BlockCompressedCrystal extends Block {
@@ -13,6 +18,10 @@ public class BlockCompressedCrystal extends Block {
         setRegistryName("blockcompressedcrystal");
     }
 
+    @SideOnly(Side.CLIENT)
+    public void initModel() {
+        ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+    }
 
 
 }
