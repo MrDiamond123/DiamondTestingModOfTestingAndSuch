c2b2471321f71ef2d88a9ac1f2a2afd041284ea3
Mr_Diamond123 <kurtisiscool123@gmail.com>
2018-07-17 11:39:20 -0400

    FINALLY HOME YAY

--- src/main/java/diamond/dtmotas/ModItems.java
+++ src/main/java/diamond/dtmotas/ModItems.java
@@ -7,6 +7,7 @@ import diamond.dtmotas.items.tools.ToolSpade;
 import diamond.dtmotas.items.tools.ToolSword;
 
 import net.minecraft.init.SoundEvents;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
 
@@ -24,23 +25,23 @@ public class ModItems {
 
 
     // MATERIALS
-
     public static final Item.ToolMaterial MATERIAL_CRYSTAL = EnumHelper.addToolMaterial("material_crystal", 4, 2000, 10.0F, 4.5F, 22);
-    public static final ItemArmor.ArmorMaterial ARMOR_MATERIAL_CRYSTAL = EnumHelper.addArmorMaterial("armor_material_crystal",ModDtmotas.MODID + "crystal",15, new int[] {4, 9, 7, 4}, 30, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 10F);
+    public static final ItemArmor.ArmorMaterial ARMOR_MATERIAL_CRYSTAL = EnumHelper.addArmorMaterial("armor_material_crystal",ModDtmotas.MODID + "crystal",44, new int[] {4, 7, 9, 4}, 30, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 10F);
+
     // ITEMS
-
-
-
     public static final Item CRYSTAL = new ItemBase("itemcrystal");
 
+    // TOOLS
     public static final ToolAxe CRYSTAL_AXE = new ToolAxe("axecrystal", MATERIAL_CRYSTAL);
-
     public static final ToolSword CRYSTAL_SWORD = new ToolSword("swordcrystal", MATERIAL_CRYSTAL);
-
     public static final ToolPickaxe CRYSTAL_PICKAXE = new ToolPickaxe("pickaxecrystal", MATERIAL_CRYSTAL);
-
     public static final ToolSpade CRYSTAL_SPADE = new ToolSpade("shovelcrystal", MATERIAL_CRYSTAL);
 
+    //ARMOR
+    public static final Item CRYSTAL_HELMET = new ArmorBase("helmetcrystal", ARMOR_MATERIAL_CRYSTAL, 1, EntityEquipmentSlot.HEAD);
+    public static final Item CRYSTAL_CHESTPLATE = new ArmorBase("chestplatecrystal", ARMOR_MATERIAL_CRYSTAL, 1, EntityEquipmentSlot.CHEST);
+    public static final Item CRYSTAL_LEGGINGS = new ArmorBase("leggingscrystal", ARMOR_MATERIAL_CRYSTAL, 2, EntityEquipmentSlot.LEGS);
+    public static final Item CRYSTAL_BOOTS = new ArmorBase("bootscrystal", ARMOR_MATERIAL_CRYSTAL, 1, EntityEquipmentSlot.FEET);
 
 
 }
--- src/main/java/diamond/dtmotas/items/ArmorBase.java
+++ src/main/java/diamond/dtmotas/items/ArmorBase.java
@@ -1,4 +1,27 @@
 package diamond.dtmotas.items;
 
-public class ArmorBase {
+import diamond.dtmotas.ModDtmotas;
+import diamond.dtmotas.ModItems;
+import diamond.dtmotas.util.IHasModel;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemArmor;
+
+public class ArmorBase extends ItemArmor implements IHasModel {
+
+    public ArmorBase(String name, ArmorMaterial materialIn, int renderIndexIn, EntityEquipmentSlot equipmentSlotIn) {
+        super(materialIn, renderIndexIn, equipmentSlotIn);
+        setUnlocalizedName(name);
+        setRegistryName(name);
+        setCreativeTab(CreativeTabs.COMBAT);
+
+        ModItems.ITEMS.add(this);
+    }
+
+    @Override
+    public void registerModels()
+    {
+        ModDtmotas.proxy.registerItemRenderer(this, 0, "inventory");
+    }
+
 }
